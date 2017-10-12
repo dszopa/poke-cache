ce71015dff1320690bfce2de107c11fec59ce840
dszopa <szopanator@gmail.com>
2017-10-12 16:02:06 -0500

    Implement DbConnectionService, Ignore config.properties

--- src/main/java/service/DbConnectionService.java
+++ src/main/java/service/DbConnectionService.java
@@ -3,9 +3,13 @@ package service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConnectionService{
 
@@ -16,14 +20,36 @@ public class DbConnectionService{
         if (connection == null) {
             synchronized (DbConnectionService.class) {
                 if (connection == null) {
+
+                    Properties properties = new Properties();
+                    InputStream inputStream = null;
+
                     try {
-                        // TODO: need to add driver
-                        connection = DriverManager.getConnection("jdbc:mysql://localhost:test?" +
-                                "user=minty&password=greatsqldb");
-                    } catch (SQLException e) {
-                        // TODO: There was an error in trying to connect to the database, make sure to log it.
-                        logger.error("Failed to connect to database!", e);
-                        connection = null;
+                        inputStream = new FileInputStream("src/main/resources/config.properties");
+                        properties.load(inputStream);
+
+                        String url = properties.getProperty("database.url");
+                        String username = properties.getProperty("database.username");
+                        String password = properties.getProperty("database.password");
+                        String connectionString = "jdbc:mysql://" + url + "?user=" + username + "&password=" + password;
+
+                        try {
+                            // Load Driver
+                            Class.forName("com.mysql.jdbc.Driver");
+                            connection = DriverManager.getConnection(connectionString);
+                        } catch (SQLException | ClassNotFoundException e) {
+                            logger.error("Failed to connect to database!", e);
+                        }
+                    } catch (Exception e) {
+                        logger.error("Failed to load \"config.properties\".", e);
+                    } finally {
+                        if (inputStream != null) {
+                            try {
+                                inputStream.close();
+                            } catch (IOException e) {
+                                logger.error("Failed to close input stream.", e);
+                            }
+                        }
                     }
                 }
             }
