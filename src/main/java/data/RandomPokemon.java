b0c4af5066932829590cf5b241fcc9a2a7a1e929
dszopa <szopanator@gmail.com>
2017-10-24 15:19:59 -0500

    Add & Use RandomPokemonBuilder

--- src/main/java/data/RandomPokemon.java
+++ src/main/java/data/RandomPokemon.java
@@ -222,4 +222,142 @@ public class RandomPokemon implements Serializable {
     public void setMove4(String move4) {
         this.move4 = move4;
     }
+
+
+    public static final class RandomPokemonBuilder {
+        private Long id;
+        private String name;
+        private String nickname;
+        private String item;
+        private String ability;
+        private Integer level;
+        // Typing
+        private String type1;
+        private String type2;
+        // Stats
+        private Integer hp;
+        private Integer attack;
+        private Integer defence;
+        private Integer specialAttack;
+        private Integer specialDefence;
+        private Integer speed;
+        // Move names
+        private String move1;
+        private String move2;
+        private String move3;
+        private String move4;
+
+        public RandomPokemonBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public RandomPokemonBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public RandomPokemonBuilder withNickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+
+        public RandomPokemonBuilder withItem(String item) {
+            this.item = item;
+            return this;
+        }
+
+        public RandomPokemonBuilder withAbility(String ability) {
+            this.ability = ability;
+            return this;
+        }
+
+        public RandomPokemonBuilder withLevel(Integer level) {
+            this.level = level;
+            return this;
+        }
+
+        public RandomPokemonBuilder withType1(String type1) {
+            this.type1 = type1;
+            return this;
+        }
+
+        public RandomPokemonBuilder withType2(String type2) {
+            this.type2 = type2;
+            return this;
+        }
+
+        public RandomPokemonBuilder withHp(Integer hp) {
+            this.hp = hp;
+            return this;
+        }
+
+        public RandomPokemonBuilder withAttack(Integer attack) {
+            this.attack = attack;
+            return this;
+        }
+
+        public RandomPokemonBuilder withDefence(Integer defence) {
+            this.defence = defence;
+            return this;
+        }
+
+        public RandomPokemonBuilder withSpecialAttack(Integer specialAttack) {
+            this.specialAttack = specialAttack;
+            return this;
+        }
+
+        public RandomPokemonBuilder withSpecialDefence(Integer specialDefence) {
+            this.specialDefence = specialDefence;
+            return this;
+        }
+
+        public RandomPokemonBuilder withSpeed(Integer speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public RandomPokemonBuilder withMove1(String move1) {
+            this.move1 = move1;
+            return this;
+        }
+
+        public RandomPokemonBuilder withMove2(String move2) {
+            this.move2 = move2;
+            return this;
+        }
+
+        public RandomPokemonBuilder withMove3(String move3) {
+            this.move3 = move3;
+            return this;
+        }
+
+        public RandomPokemonBuilder withMove4(String move4) {
+            this.move4 = move4;
+            return this;
+        }
+
+        public RandomPokemon build() {
+            RandomPokemon randomPokemon = new RandomPokemon();
+            randomPokemon.setId(id);
+            randomPokemon.setName(name);
+            randomPokemon.setNickname(nickname);
+            randomPokemon.setItem(item);
+            randomPokemon.setAbility(ability);
+            randomPokemon.setLevel(level);
+            randomPokemon.setType1(type1);
+            randomPokemon.setType2(type2);
+            randomPokemon.setHp(hp);
+            randomPokemon.setAttack(attack);
+            randomPokemon.setDefence(defence);
+            randomPokemon.setSpecialAttack(specialAttack);
+            randomPokemon.setSpecialDefence(specialDefence);
+            randomPokemon.setSpeed(speed);
+            randomPokemon.setMove1(move1);
+            randomPokemon.setMove2(move2);
+            randomPokemon.setMove3(move3);
+            randomPokemon.setMove4(move4);
+            return randomPokemon;
+        }
+    }
 }
--- src/main/java/factory/RandomPokemonFactory.java
+++ src/main/java/factory/RandomPokemonFactory.java
@@ -6,11 +6,25 @@ import dto.RandomPokemonDTO;
 public class RandomPokemonFactory {
 
     public RandomPokemon createRandomPokemon(RandomPokemonDTO randomPokemonDTO) {
-        return new RandomPokemon(randomPokemonDTO.getId(), randomPokemonDTO.getName(), randomPokemonDTO.getNickname(),
-                randomPokemonDTO.getItem(), randomPokemonDTO.getAbility(), randomPokemonDTO.getLevel(),
-                randomPokemonDTO.getType1(), randomPokemonDTO.getType2(), randomPokemonDTO.getHp(),
-                randomPokemonDTO.getAttack(), randomPokemonDTO.getDefence(), randomPokemonDTO.getSpecialAttack(),
-                randomPokemonDTO.getSpecialDefence(), randomPokemonDTO.getSpeed(), randomPokemonDTO.getMove1(),
-                randomPokemonDTO.getMove2(), randomPokemonDTO.getMove3(), randomPokemonDTO.getMove4());
+        return new RandomPokemon.RandomPokemonBuilder()
+                .withId(randomPokemonDTO.getId())
+                .withName(randomPokemonDTO.getName())
+                .withNickname(randomPokemonDTO.getNickname())
+                .withItem(randomPokemonDTO.getItem())
+                .withAbility(randomPokemonDTO.getAbility())
+                .withLevel(randomPokemonDTO.getLevel())
+                .withType1(randomPokemonDTO.getType1())
+                .withType2(randomPokemonDTO.getType2())
+                .withHp(randomPokemonDTO.getHp())
+                .withAttack(randomPokemonDTO.getAttack())
+                .withDefence(randomPokemonDTO.getDefence())
+                .withSpecialAttack(randomPokemonDTO.getSpecialAttack())
+                .withSpecialDefence(randomPokemonDTO.getSpecialDefence())
+                .withSpeed(randomPokemonDTO.getSpeed())
+                .withMove1(randomPokemonDTO.getMove1())
+                .withMove2(randomPokemonDTO.getMove2())
+                .withMove3(randomPokemonDTO.getMove3())
+                .withMove4(randomPokemonDTO.getMove4())
+                .build();
     }
 }
